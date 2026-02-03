881044ff0987456fe686da10a5250f502b570e81
Artem Zyktin <zyktin.a.a@gmail.com>
2026-02-04 07:30:49 +0800

    Fix header-static issue

--- src/ecs/entity_id.h
+++ src/ecs/entity_id.h
@@ -7,7 +7,7 @@ namespace ecs
 {
 
 using entity_id = uint32_t;
-static constexpr entity_id INVALID_ENTITY_ID = std::numeric_limits<entity_id>::max();
-static constexpr entity_id MAX_ENTITY_COUNT = 1024 * 512;
+inline constexpr entity_id INVALID_ENTITY_ID = std::numeric_limits<entity_id>::max();
+inline constexpr entity_id MAX_ENTITY_COUNT = 1024 * 512;
 
 }
