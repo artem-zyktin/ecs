dc8df2d23fdff7c48030450720bfdec76a842fba
Artem Zyktin <zyktin.a.a@gmail.com>
2026-02-25 13:10:49 +0800

    Add ecs component concept

---
+++ src/include/ecs/component_concept.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+
+#include "ecs/abstract_component.h"
+
+#include <type_traits>
+
+namespace ecs
+{
+
+template<typename T>
+concept ecs_component = requires
+{
+	typename T::value_type;
+	requires std::derived_from<T, abstract_component<typename T::value_type>>;
+}
+&& std::is_nothrow_default_constructible_v<T>
+&& std::is_nothrow_destructible_v<T>;
+
+} // namespace ecs
