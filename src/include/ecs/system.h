a57cb103e193194682c10f80ae7fafe87ce02337
Artem Zyktin <zyktin.a.a@gmail.com>
2026-03-02 17:40:25 +0800

    Add base_system ecs

---
+++ src/include/ecs/system.h
@@ -0,0 +1,34 @@
+﻿#pragma once
+
+#include "ecs/component_concept.h"
+
+#include <tuple>
+
+namespace ecs
+{
+
+template<typename T, typename... component_t>
+concept is_ecs_system = requires (T system, component_t*... ptrs)
+{
+	system.proc(ptrs...);
+};
+
+
+template<typename derived_t, ecs_component... component_t>
+requires is_ecs_system<derived_t, component_t...>
+struct system_base
+{
+	void run(float delta_time);
+	void set(component_t*... ptrs) noexcept;
+
+protected:
+
+	virtual void proc(component_t*... ptr) = 0;
+
+private:
+	std::tuple<component_t*...> components_;
+};
+
+} // namespace ecs
+
+#include "ecs/system.hpp"
---
+++ src/include/ecs/system.hpp
@@ -0,0 +1,27 @@
+﻿#include "system.h"
+#pragma once
+
+namespace ecs
+{
+
+template<typename derived_t, ecs_component ...component_t>
+requires is_ecs_system<derived_t, component_t...>
+inline void ecs::system_base<derived_t, component_t...>::run(float delta_time)
+{
+	std::apply([this](component_t*... ptrs)
+	{
+		static_cast<derived_t*>(this)->proc(ptrs...);
+	},
+	components_);
+}
+
+template<typename derived_t, ecs_component ...component_t>
+requires is_ecs_system<derived_t, component_t...>
+inline void system_base<derived_t, component_t...>::set(component_t* ...ptrs) noexcept
+{
+	components_ = { ptrs... };
+}
+
+
+
+} // namespace ecs
